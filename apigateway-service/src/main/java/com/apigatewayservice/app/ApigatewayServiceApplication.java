1529b2401a1f2fdd7eee4495e43625e6d9243334
Nahuel Tabasso <nahueltabasso@gmail.com>
2023-07-23 13:11:01 -0300

    Build Docker image to apigateway-service

--- apigateway-service/src/main/java/com/apigatewayservice/app/ApigatewayServiceApplication.java
+++ apigateway-service/src/main/java/com/apigatewayservice/app/ApigatewayServiceApplication.java
@@ -2,12 +2,17 @@ package com.apigatewayservice.app;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class ApigatewayServiceApplication {
+
+    @Autowired
+    private Environment environment;
 
     public static void main(String[] args) {
         SpringApplication.run(ApigatewayServiceApplication.class, args);
@@ -15,6 +20,9 @@ public class ApigatewayServiceApplication {
 
     @Bean
     public ObjectMapper objectMapper() {
+        System.out.println(environment.getProperty("AUTH_SERVICE_URI"));
+        System.out.println(environment.getProperty("USER_SERVICE_URI"));
+        System.out.println(environment.getProperty("app.security.validate-token-uri"));
         return new ObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL);
     }
 
