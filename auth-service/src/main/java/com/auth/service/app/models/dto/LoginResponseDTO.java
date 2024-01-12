7fa8dd6db7e176604064175d0e57e8f63a04b886
Nahuel Tabasso <nahueltabasso@gmail.com>
2024-01-11 22:24:01 -0300

    fix: resolve bug - update firstLogin property after when user enter to the app

--- auth-service/src/main/java/com/auth/service/app/models/dto/LoginResponseDTO.java
+++ auth-service/src/main/java/com/auth/service/app/models/dto/LoginResponseDTO.java
@@ -4,10 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
 
 import java.time.LocalDateTime;
-import java.util.Collection;
 import java.util.List;
 
 @Data
@@ -25,4 +23,5 @@ public class LoginResponseDTO {
     private String refreshToken;
     private String type = "Bearer";
     private LocalDateTime currentDateTime;
+    private Boolean firstLogin;
 }
